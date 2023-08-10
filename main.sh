c336203769d1bee1823e3113dda5111fe676474f
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-10 14:48:28 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,25 @@
+apt update -y
+docker run --cap-add=NET_ADMIN -it ubuntu:20.04
+apt update -y
+apt-get install iptables sudo -y
+sudo apt install ubuntu-desktop
+sudo apt install udisks2
+sudo rm /var/lib/dpkg/info/udisks2.postinst
+sudo dpkg --configure udisks2
+sudo apt install xfce4 xfce4-terminal
+sudo apt install curl
+
+ sudo apt update
+ sudo apt-get install gnome-panel gnome-settings-daemon metacity nautilus gnome-terminal
+ sudo apt install -y tightvncserver nano net-tools
+ sudo vncserver
+ 
+sudo nano ~/.vnc/xstartup
+
+
+sudo vncserver -kill :1
+sudo vncserver
+
+sudo apt install novnc
+
+websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 6080 localhost:5901
