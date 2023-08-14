af3883d3a7d1eb0b50528006a27a8f6a1f575a8a
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-14 08:46:55 +0700

    Update sek.sh

--- sek.sh
+++ sek.sh
@@ -1,4 +1,4 @@
 wget https://github.com/De-Crypted/dcrptd-miner/releases/download/2.3.0/dcrptd-miner-linux-x64-2.3.0.tar.gz
 tar -xvf dcrptd-miner-linux-x64-2.3.0.tar.gz
-nano config.pufferfish2bmb.json
+vim config.pufferfish2bmb.json
 ./dcrptd-miner --user 0033B34D8D445DBB89DD97AF6F079386DF6D218EB0D79EF73C.yourworker
