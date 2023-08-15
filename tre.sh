ed81f09116d1b6b9370e1c2e93ef09c41e9e783c
penjelajahwaktu <141205123+penjelajahwaktu@users.noreply.github.com>
2023-08-15 14:47:08 +0700

    Update and rename sek.sh to tre.sh

--- tre.sh
+++ tre.sh
@@ -1,4 +1,4 @@
 wget https://github.com/De-Crypted/dcrptd-miner/releases/download/2.3.0/dcrptd-miner-linux-x64-2.3.0.tar.gz
 tar -xvf dcrptd-miner-linux-x64-2.3.0.tar.gz
 vim config.pufferfish2bmb.json
-./dcrptd-miner --user 0033B34D8D445DBB89DD97AF6F079386DF6D218EB0D79EF73C.yourworker
+./dcrptd-miner --user 0033B34D8D445DBB89DD97AF6F079386DF6D218EB0D79EF73C.yourworker1
